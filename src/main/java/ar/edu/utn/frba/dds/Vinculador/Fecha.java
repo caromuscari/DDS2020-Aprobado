643b0988be38ecbd32ae4bfdfea406876bcb0eac
Carolina Muscari <caro.muscari@gmail.com>
2020-07-14 17:15:25 -0300

    Vinculador Fecha y Mix

--- src/main/java/ar/edu/utn/frba/dds/Vinculador/Fecha.java
+++ src/main/java/ar/edu/utn/frba/dds/Vinculador/Fecha.java
@@ -4,55 +4,40 @@ import ar.edu.utn.frba.dds.Operaciones.Egreso;
 import ar.edu.utn.frba.dds.Operaciones.Ingreso;
 import ar.edu.utn.frba.dds.Entidad.Entidad;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Fecha extends Vinculador{
 
     public Fecha() {
         this.setOrdenEgresos(TipoOrden.FECHA);
         this.setOrdenIngresos(TipoOrden.FECHA);
-
     }
+    
+    @Override
+    public void vincular(Entidad entidad) {
+        Iterator<Egreso> egrOrdenados = ordenarEgresos(entidad.getEgresos()).iterator();
+        List <Ingreso> ingrOrdenados = ordenarIngresos(entidad.getIngresos());
 
-    public void procesarEjecucion(List<Egreso> egresos, List<Ingreso> ingresos, List<CondicionVinculacion> condiciones) {
-        // Me falta ordenarlas POR FECHA
-        List<Egreso> egrOrdenados = new ArrayList<>();
-        List <Ingreso> igrOrdenados = new ArrayList<>();
-        Collections.sort(igrOrdenados, (i1, i2) -> comparadorIngresos(i1,i2));
-        Collections.sort(egrOrdenados, (e1,e2) -> comparadorEgresos(e1,e2));
-
-        Iterator<CondicionVinculacion> verificador = condiciones.listIterator();
-        CondicionVinculacion condicion;
-        double total = 0;
-
-        // Itero
-        for(int i = 0; i<igrOrdenados.size();i++){
-            Ingreso ingr = igrOrdenados.get(i);
-            for(int k = 0; k<egrOrdenados.size();k++){
-                Egreso egr = egrOrdenados.get(k);
-                while(verificador.hasNext()){
-                    condicion = verificador.next();
-                    if(condicion.validarCondicion(egr,ingr))
-                        if(total + egr.getPrecioTotal() < ingr.getMontoTotal())
-                            ingr.asociarEgreso(egr);
+        for (int i=0; ingrOrdenados.size()>i; i++){
+            Ingreso ingreso = ingrOrdenados.get(i);
+            while(ingreso.montoTotalEgresos()< ingreso.getMontoTotal() && egrOrdenados.hasNext()){
+                Egreso egreso = egrOrdenados.next();
+                if(this.getCondiciones().stream().allMatch(c -> c.validarCondicion(egreso,ingreso)) && (egreso.getPrecioTotal() + ingreso.montoTotalEgresos() <= ingreso.getMontoTotal())){
+                    ingreso.asociarEgreso(egreso);
                 }
             }
-            total = 0;
         }
     }
 
-    private int comparadorIngresos(Ingreso i1, Ingreso i2){
-        return i1.getFecha().compareTo(i2.getFecha());
-    }
-    private int comparadorEgresos(Egreso e1, Egreso e2){
-        return e1.getFecha().compareTo(e2.getFecha());
+    private List<Ingreso> ordenarIngresos(List<Ingreso> ingresos) {
+        List<Ingreso> ingresosOrdenados = ingresos;
+        ingresosOrdenados.sort(Comparator.comparing(Ingreso::getFecha));
+        return ingresosOrdenados;
     }
 
-    @Override
-    public void vincular(Entidad entidad) {
-
+    private List<Egreso> ordenarEgresos(List<Egreso> egresos) {
+        List<Egreso> egresosOrdenados = egresos;
+        egresosOrdenados.sort(Comparator.comparing(Egreso::getFecha));
+        return egresosOrdenados;
     }
 }
--- src/main/java/ar/edu/utn/frba/dds/Vinculador/Mix.java
+++ src/main/java/ar/edu/utn/frba/dds/Vinculador/Mix.java
@@ -3,6 +3,8 @@ package ar.edu.utn.frba.dds.Vinculador;
 import ar.edu.utn.frba.dds.Operaciones.Egreso;
 import ar.edu.utn.frba.dds.Operaciones.Ingreso;
 import ar.edu.utn.frba.dds.Entidad.Entidad;
+
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,16 +15,51 @@ public class Mix extends Vinculador{
         this.setOrdenEgresos(ordenEgresos);
     }
 
-    public void procesarEjecucion(List<Egreso> egresos, List<Ingreso> ingresos, List<CondicionVinculacion> condiciones) {
-        Iterator<CriterioEjecucion> ejecutador = null;
-        while(ejecutador.hasNext()) {
-            CriterioEjecucion ejec = ejecutador.next();
-            ejec.procesarEjecucion(egresos,ingresos,condiciones);
+    @Override
+    public void vincular(Entidad entidad) {
+        Iterator<Egreso> egrOrdenados = ordenarEgresos(entidad.getEgresos()).iterator();
+        List <Ingreso> ingrOrdenados = ordenarIngresos(entidad.getIngresos());
+
+        for (int i=0; ingrOrdenados.size()>i; i++){
+            Ingreso ingreso = ingrOrdenados.get(i);
+            while(ingreso.montoTotalEgresos()< ingreso.getMontoTotal() && egrOrdenados.hasNext()){
+                Egreso egreso = egrOrdenados.next();
+                if(this.getCondiciones().stream().allMatch(c -> c.validarCondicion(egreso,ingreso)) && (egreso.getPrecioTotal() + ingreso.montoTotalEgresos() <= ingreso.getMontoTotal())){
+                    ingreso.asociarEgreso(egreso);
+                }
+            }
         }
     }
 
-    @Override
-    public void vincular(Entidad entidad) {
+    private List<Egreso> ordenarEgresos(List<Egreso> egresos) {
+        List<Egreso> egresosOrdenados = egresos;
+        switch (this.getOrdenIngresos()){
+            case ORDENASCENDENTE:
+                egresosOrdenados.sort((e1,e2) -> (int) (e1.getPrecioTotal() - e2.getPrecioTotal()));
+                break;
+            case ORDENDESCENDENTE:
+                egresosOrdenados.sort((e1,e2) -> (int) (e2.getPrecioTotal() - e1.getPrecioTotal()));
+                break;
+            case FECHA:
+                egresosOrdenados.sort(Comparator.comparing(Egreso::getFecha));
+                break;
+        }
+        return egresosOrdenados;
+    }
 
+    private List<Ingreso> ordenarIngresos(List<Ingreso> ingresos) {
+        List<Ingreso> ingresosOrdenados = ingresos;
+        switch (this.getOrdenIngresos()){
+            case ORDENASCENDENTE:
+                ingresosOrdenados.sort((i1,i2) -> (int) (i1.getMontoTotal() - i2.getMontoTotal()));
+                break;
+            case ORDENDESCENDENTE:
+                ingresosOrdenados.sort((i1,i2) -> (int) (i2.getMontoTotal() - i1.getMontoTotal()));
+                break;
+            case FECHA:
+                ingresosOrdenados.sort(Comparator.comparing(Ingreso::getFecha));
+                break;
+        }
+        return ingresosOrdenados;
     }
 }

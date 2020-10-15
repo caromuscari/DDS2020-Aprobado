ee053ac85c172bef76de42d9ba7c3ca351a34f28
Manuel Viegas <mviegas94@gmail.com>
2020-10-15 20:08:46 -0300

    API para obtener mensajes funcionando

--- src/main/java/ar/edu/utn/frba/dds/Controladores/LicitacionController.java
+++ src/main/java/ar/edu/utn/frba/dds/Controladores/LicitacionController.java
@@ -1,22 +1,18 @@
 package ar.edu.utn.frba.dds.Controladores;
 
-import ar.edu.utn.frba.dds.BandejaDeMendajes.Mensaje;
 import ar.edu.utn.frba.dds.DTO.MensajeDTO;
 import ar.edu.utn.frba.dds.Repositorios.LicitacionRepo;
 import ar.edu.utn.frba.dds.Repositorios.RepoUsuarios;
-import ar.edu.utn.frba.dds.ResultadoLicitacion.EstadoValidacion;
-import ar.edu.utn.frba.dds.ResultadoLicitacion.ResultadoValidacion;
 import ar.edu.utn.frba.dds.Usuario.Usuario;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ar.edu.utn.frba.dds.Licitacion.Licitacion;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 
-import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -36,32 +32,50 @@ public class LicitacionController {
     public static Object validarLicitacion(Request request, Response response) {
         String nombreLicitacion = request.queryMap("nombreLicitacion").value();
         Optional<Licitacion> licitacion = LicitacionRepo.find(nombreLicitacion);
-        if (licitacion.isPresent()){
+        if (licitacion.isPresent()) {
             licitacion.get().validarLicitacion();
         }
         return null;
     }
 
-    public static Object mostrarMensajes(Request request, Response response) throws JsonProcessingException {
+    public static Object mostrarMensajes(Request request, Response response) {
         String licitacion = request.queryMap("nombreLicitacion").value();
         String nombreUsuario = request.queryMap("usuario").value();
-        Usuario usuario = repo.buscarUsuario(nombreUsuario);
-        if(usuario != null){
-            List<MensajeDTO> listaMensajes = usuario.getBandejaDeMensajes().stream()
-                    .filter(l -> l.getResultados().get(0).getLicitacion().getNombre().matches(licitacion))
-                    .map(l -> new MensajeDTO(l.obtenerMensaje(),l.getLeido()))
-                    .collect(Collectors.toList());
 
-            Gson gson = new Gson();
-            String mensajesJson = gson.toJson(listaMensajes);
-            try {
-                PrintWriter out = response.raw().getWriter();
-                response.header("Content-Type","application/json");
-                out.print(mensajesJson);
-                out.flush();
-            } catch (IOException e){
-                System.out.println("error convirtiendo mensajes : " + e.getMessage());
+        try {
+            PrintWriter writer = response.raw().getWriter();
+            response.header("Content-Type", "application/json");
+
+            if (licitacion == null || nombreUsuario == null) {
+                ObjectNode ob = JsonNodeFactory.instance.objectNode();
+                ob.put("Error", "No se envio la licitacion o el usuario como queryParam");
+                writer.print(ob);
+                writer.flush();
+                return null;
             }
+
+            Usuario usuario = repo.buscarUsuario(nombreUsuario);
+
+            if (usuario != null) {
+                List<MensajeDTO> listaMensajes = usuario.getBandejaDeMensajes().stream()
+                        .filter(l -> l.getResultados().get(0).getLicitacion().getNombre().matches(licitacion))
+                        .map(l -> new MensajeDTO(l.obtenerMensaje(), l.getLeido()))
+                        .collect(Collectors.toList());
+
+                Gson gson = new Gson();
+                String mensajesJson = gson.toJson(listaMensajes);
+                writer.print(mensajesJson);
+                writer.flush();
+                return null;
+            } else {
+                ObjectNode ob = JsonNodeFactory.instance.objectNode();
+                ob.put("Erorr", "No se encontro el usuario " + nombreUsuario);
+                writer.print(ob);
+                writer.flush();
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
